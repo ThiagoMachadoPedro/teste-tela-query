c75036e4f513ddd50a59488e966a7d08e64c3c11
Thiago Machado Pedro <94803123+ThiagoMachadoPedro@users.noreply.github.com>
2023-08-21 19:23:01 -0300

    teste realizado

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/ThiagoMachadoPedro')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(json) {
+            nameElement.innerHTML = json.name;
+            usernameElement.innerHTML = json.login;
+            avatarElement.src = json.avatar_url;
+            followingElement.innerHTML = json.following;
+            followersElement.innerHTML = json.followers;
+            reposElement.innerHTML = json.public_repos;
+            linkElement.href = json.html_url;
+        });
+});
